efa7df9095dad2345a74eeb9efc9e95160bb9d1f
Martin Nagy <martin.laszlo.nagy@gmail.com>
2022-03-22 23:28:31 +0100

    Add hook for testing.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import useFetchDictionary from './services/useFetchDictionary'
 
 function App() {
+  const {data: dictionary, isPending, error} = useFetchDictionary('https://localhost:44323/Dictionary');
   return (
-    <></>
+    <>{dictionary && dictionary.map(dictionaryEntry => dictionaryEntry.hungarian)}</>
   );
 }
 
